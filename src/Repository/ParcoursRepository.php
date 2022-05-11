4971836b7cb40981dca4bc33479eac0b0f34ecbf
davidannebicque <david.annebicque@gmail.com>
2022-05-11 17:43:59 +0200

    feat: Offre/Parcours

--- src/Repository/ParcoursRepository.php
+++ src/Repository/ParcoursRepository.php
@@ -13,7 +13,6 @@ use Doctrine\Persistence\ManagerRegistry;
  *
  * @method Parcours|null find($id, $lockMode = null, $lockVersion = null)
  * @method Parcours|null findOneBy(array $criteria, array $orderBy = null)
- * @method Parcours[]    findAll()
  * @method Parcours[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ParcoursRepository extends ServiceEntityRepository
@@ -23,10 +22,17 @@ class ParcoursRepository extends ServiceEntityRepository
         parent::__construct($registry, Parcours::class);
     }
 
-    /**
-     * @throws ORMException
-     * @throws OptimisticLockException
-     */
+    public function findAll() {
+        return $this->createQueryBuilder('p')
+            ->join('p.diplome', 'd')
+            ->addSelect('d')
+            ->orderBy('d.libelle', 'ASC')
+            ->addOrderBy('p.libelle', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+
     public function add(Parcours $entity, bool $flush = true): void
     {
         $this->_em->persist($entity);
@@ -35,10 +41,6 @@ class ParcoursRepository extends ServiceEntityRepository
         }
     }
 
-    /**
-     * @throws ORMException
-     * @throws OptimisticLockException
-     */
     public function remove(Parcours $entity, bool $flush = true): void
     {
         $this->_em->remove($entity);
@@ -46,33 +48,4 @@ class ParcoursRepository extends ServiceEntityRepository
             $this->_em->flush();
         }
     }
-
-    // /**
-    //  * @return Parcours[] Returns an array of Parcours objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Parcours
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
