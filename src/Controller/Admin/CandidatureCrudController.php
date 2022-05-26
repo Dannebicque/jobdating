d0c9fd17d2daedc27c4de91b026adcc34cff0132
davidannebicque <david.annebicque@gmail.com>
2022-05-26 09:25:12 +0200

    fix: Planning des entreprises

--- src/Controller/Admin/CandidatureCrudController.php
+++ src/Controller/Admin/CandidatureCrudController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -25,8 +26,28 @@ class CandidatureCrudController extends AbstractCrudController
             IdField::new('id')->hideOnIndex(),
             AssociationField::new('offre', 'Offre')->setCrudController(OffreCrudController::class),
             DateTimeField::new('creneau')->setFormat('none', 'HH:mm'),
-            TextField::new('hasCV'),
-            TextField::new('hasLettre'),
+            ChoiceField::new('hasCV')->renderAsBadges(
+                [
+                    'CV Déposé' => 'success',
+                    'Pas de CV' => 'warning',
+                ]
+            )->setChoices(
+                [
+                    'CV Déposé' => 'CV Déposé',
+                    'Pas de CV' => 'Pas de CV',
+                ]
+            ),
+            ChoiceField::new('hasLettre')->renderAsBadges(
+                [
+                    'Lettre Déposée' => 'success',
+                    'Pas de Lettre' => 'warning',
+                ]
+            )->setChoices(
+                [
+                    'Lettre Déposée' => 'Lettre Déposée',
+                    'Pas de Lettre' => 'Pas de Lettre',
+                ]
+            ),
             AssociationField::new('etudiant', 'Etudiant')->setCrudController(EtudiantCrudController::class),
 
         ];
